777c1eabe9c22b4593508c5633303cf44bb9a0cc
clbk <apenasjogos@yahoo.com.br>
2020-05-15 18:24:49 -0300

    primeiro componente  com propriedade(app-header)

--- tutorial3/src/app/components/components.module.ts
+++ tutorial3/src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
@@ -9,8 +10,15 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    //imprtar os componentes do ionic para o header component funcionar
+    IonicModule,
+  ],
+  //acrescentado o export do modulo do componente header criado
+  exports:[
+    HeaderComponent
   ]
+  
 })
 export class ComponentsModule { }
 //1:05
--- tutorial3/src/app/pages/alert/alert.module.ts
+++ tutorial3/src/app/pages/alert/alert.module.ts
@@ -7,13 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { AlertPageRoutingModule } from './alert-routing.module';
 
 import { AlertPage } from './alert.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AlertPageRoutingModule
+    AlertPageRoutingModule,
+    //modulo dos componentes criados
+    ComponentsModule
   ],
   declarations: [AlertPage]
 })
