7254ff835860a846ae117301d2e91cb7be8e7afe
clbk <apenasjogos@yahoo.com.br>
2020-05-16 08:36:56 -0300

    lista com item clicavel

--- tutorial3/src/app/pages/inicio/inicio.page.ts
+++ tutorial3/src/app/pages/inicio/inicio.page.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  /*array de Componente-> interface criada depois desta classe*/
+  componentes:any[]=[
+    {
+      icon:"american-football",
+      nome:"Action",
+      irPara:"/action-sheet",
+    },
+    {
+      icon:"appstore",
+      nome:"Alert",
+      irPara:"/alert",
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
-  }
+  }  
 
+}
+
+interface Componente{
+  icon:string;
+  nome:string;
+  irPara:string;
 }
